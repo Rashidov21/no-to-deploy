f601037a26714bc76a36a63c44a8553128c8d75c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-23 16:43:50 +0500

    update script

--- js-code/script.js
+++ js-code/script.js
@@ -1,30 +1,30 @@
-import EditorJS from '@editorjs/editorjs';
+
+// task 1
+// Uch xonali son berilgan.
+// Uning raqamlari o‘suvchi ketma-ketlik bo'lishi tekshirilsin.
+// 1 , 2 , 4  >> true
+// 1 ,3 , 2 >> false
+// task 2
+// To‘rt xonali son berilgan. Uni chapdan o‘ngga va o‘ngdan chapga
+// o‘qiganda bir xil o‘qilishi tekshirilsin.
+// 1221 >> true
+// 1232 >> false
+// task 3
+// Userdan 3 ta son qabul qiling ularni to'g'ri joylashtirish orqali
+// sonlardan  eng katta qiymatni xosil qiling
+// 1,7,3 >> 731
 
 
-const editor = new EditorJS(
-    {
-        "time": 1550476186479,
-        "blocks": [
-            {
-                "type": "paragraph",
-                "data": {
-                    "text": "The example of text that was written in <b>one of popular</b> text editors."
-                }
-            },
-            {
-                "type": "header",
-                "data": {
-                    "text": "With the header of course",
-                    "level": 2
-                }
-            },
-            {
-                "type": "paragraph",
-                "data": {
-                    "text": "So what do we have?"
-                }
-            }
-        ],
-        "version": "2.8.1"
+// console.log(prompt().split("").sort((a, b) => b - a).join(""))
+
+// task 4
+// user 5 ta son kiritadi ularning birortasi ham bir biriga teng emasligini 
+// tekshiring 
+// a = 12397 >> true
+// b = 12336 >> false 
+let n = prompt().split("")
+if (n.length == 5) {
+    for(let i = 0; i < n.length; i++){
+        
     }
-);
+}
