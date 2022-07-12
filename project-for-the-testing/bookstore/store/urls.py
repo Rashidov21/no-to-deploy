30ee3445a9e681d45a2c242048b9a72f9217f0f7
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-12 10:12:01 +0500

    update

--- project-for-the-testing/bookstore/store/urls.py
+++ project-for-the-testing/bookstore/store/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .import views
 app_name = 'store'
 urlpatterns = [
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path("wishlist/",
+         views.add_to_wishlist, name='add_to_wishlist')
 ]
--- project-for-the-testing/bookstore/store/views.py
+++ project-for-the-testing/bookstore/store/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Category, Product
 # Create your views here.
 
@@ -6,3 +8,9 @@ from .models import Category, Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'index.html', {"object_list": products})
+
+
+def add_to_wishlist(request):
+    data = json.loads(request.GET.get("data"))
+    print(data)
+    return JsonResponse({"status": 200})
