b6403cc0f975e5e1b95ac47afba77fcf023f02b6
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-15 17:12:39 +0500

    Update firebase-file-store.py

--- python-code/firebase-file-store.py
+++ python-code/firebase-file-store.py
@@ -2,13 +2,7 @@
 import pyrebase
 
 config = {
-    'apiKey': "AIzaSyBxUA0dGD003HCahLKoYWiwt3J9VGbDg_U",
-    'authDomain': "potato-5987d.firebaseapp.com",
-    'projectId': "potato-5987d",
-    'storageBucket': "potato-5987d.appspot.com",
-    'messagingSenderId': "739055637770",
-    'appId': "1:739055637770:web:f6771260e7c445bef916f3",
-    "databaseURL": "https://potato-5987d-default-rtdb.firebaseio.com/"
+
 }
 
 firebase = pyrebase.initialize_app(config)
