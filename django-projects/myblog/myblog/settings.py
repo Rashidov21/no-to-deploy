12961db36b4fc14ce7b90f451df682f59163fd2f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-09-12 16:10:24 +0500

    update project

--- django-projects/myblog/myblog/settings.py
+++ django-projects/myblog/myblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
