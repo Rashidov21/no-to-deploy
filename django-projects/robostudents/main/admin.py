65e462f771bb80c837c5fd13a408b43d4ce24153
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-09-21 17:30:16 +0500

    updtae

--- django-projects/robostudents/main/admin.py
+++ django-projects/robostudents/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Course, Student
 # Register your models here.
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'surname', 'course']
+    list_filter = ['course', 'age']
+    prepopulated_fields = {"slug": ("name", "surname")}
