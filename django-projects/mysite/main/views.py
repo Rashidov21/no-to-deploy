d26220c54647244b23c17b903be127134a3f469d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-14 17:30:42 +0500

    add new project

---
+++ django-projects/mysite/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    # return HttpResponse("<h2>Its working ..</h2>")
+    return render(request, 'index.html')
--- django-projects/mysite/mysite/settings.py
+++ django-projects/mysite/mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
