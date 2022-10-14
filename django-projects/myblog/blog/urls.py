fa53ce28703a01a055bfd311dfbdc906d891363f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-14 16:10:13 +0500

    delete django projects

--- django-projects/myblog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('detail/<int:post_id>', views.post_detail, name='post_detail')
-]
--- django-projects/robostudents/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Student, Course
-# Create your views here.
-
-
-def all_students(request):
-    students = Student.objects.all()
-    return render(request, "index.html", {"students": students})
