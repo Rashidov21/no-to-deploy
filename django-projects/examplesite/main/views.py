3a5796b9cd4d42f40eeb8556c4fceca3331f7ba6
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-28 16:45:17 +0500

    add for and if

--- django-projects/examplesite/main/views.py
+++ django-projects/examplesite/main/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 
 def homePageView(request):
-    return render(request, "index.html")
+    numbers = list(range(10))
+    data = {
+        'header': "Django template test view",
+        "content": "This is text content.",
+        "numbers": numbers
+    }
+    return render(request, "index.html", context=data)
