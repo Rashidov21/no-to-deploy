2196038b42f60cfb06bb6f6ecf5a38278534de8c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-12-09 16:03:34 +0500

    Update views.py

--- django-projects/library/main/views.py
+++ django-projects/library/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 from .models import Book, Category, Author
 
