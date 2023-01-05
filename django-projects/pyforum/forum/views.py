a4ff4108b1df5189705e701373d3cf359acbb4fb
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-05 15:55:34 +0500

    add new code

--- django-projects/pyforum/forum/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-
-
-class HomePageView(TemplateView):
-    template_name = "index.html"
