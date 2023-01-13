494ebacc7039d86648b4765a9c21c6b66b0b7a29
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-13 10:49:31 +0500

    update svelte project

---
+++ js-code/svelte_js/svelte-tutorial/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import AddTodo from "./components/AddTodo.svelte";
+	const props = {
+		title:"Add new todo",
+		btnTitle:"Go!",
+	}
+</script>
+
+<main class="container my-5">
+	<AddTodo {...props}/>
+</main>
+
+<style>
+
+</style>
---
+++ js-code/svelte_js/svelte-tutorial/src/components/AddTodo.svelte
@@ -0,0 +1,13 @@
+<script>
+export let title = "Todo text"
+export let btnTitle = "Add"
+
+</script>
+<form class="w-50 mx-auto">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">{title}</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+
+    <button type="submit" class="btn btn-primary">{btnTitle}</button>
+  </form>
