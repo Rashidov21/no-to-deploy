b0499aa390209376160a2d3052bcdf898e4ab90b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-03-27 18:00:39 +0500

    update

--- django-projects/db_managment/articles/urls.py
+++ django-projects/db_managment/articles/urls.py
@@ -5,5 +5,7 @@ app_name = 'articles'
 
 
 urlpatterns = [
-    path("", views.all_articles_view, name='post_list')
+    path("", views.all_articles_view, name='post_list'),
+    path('detail/<pk>', views.post_detail, name='detail'),
+    path("category/<slug:category_slug>/", views.category_list, name='category_list')
 ]
--- django-projects/db_managment/articles/views.py
+++ django-projects/db_managment/articles/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def all_articles_view(request):
-    return render(request, "articles/posts.html")
+    all_categories = Category.objects.all()
+    all_posts = Article.objects.all()
+    
+    data = {
+        'posts':all_posts,
+        'categories':all_categories
+    }
+    
+    return render(request, "articles/posts.html", context=data)
+
+def post_detail(request,pk):
+    article = Article.objects.get(id=pk)    
+    return render(request, 'articles/detail.html', context={"object":article})
+
+def category_list(request, category_slug):
+    category = Category.objects.get(slug=category_slug)
+    articles = Article.objects.filter(category=category)
+    return render(request, 'articles/category_posts.html', context={"posts":articles})
+    # comm
+    
