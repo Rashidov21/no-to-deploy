0db316ac3dff93c58e0ef6ea63039d8484491a43
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-04-19 16:52:27 +0500

    update player fornt

--- django-projects/db_managment/players/views.py
+++ django-projects/db_managment/players/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Player
+
 
 
 # Create your views here.
@@ -8,7 +10,11 @@ class PlayersHomeView(View):
     
     # http get method handler 
     def get(self,request):
-        return render(request, "players/players.html")
+        players = Player.objects.all()
+        data = {
+            "object_list":players
+        }
+        return render(request, "players/players.html", context=data)
     
     # http post method handler 
     def post(self,request):
