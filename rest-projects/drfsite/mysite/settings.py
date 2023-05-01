ee92af28bebe5198352e990424c2a72906b77230
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-05-01 15:29:45 +0500

    update css lesson

--- rest-projects/drfsite/mysite/settings.py
+++ rest-projects/drfsite/mysite/settings.py
@@ -123,3 +123,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# REST API 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
