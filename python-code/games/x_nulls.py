3fca25d13b523f496d70a0120fea8710e4bbd77f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-01-09 09:00:33 +0500

    Update x_nulls.py

--- python-code/games/x_nulls.py
+++ python-code/games/x_nulls.py
@@ -1,7 +1,16 @@
 import pygame 
+import sys
 
 pygame.init()
 
 game_window = pygame.display.set_mode((320,240))
 
 WHITE = (255,255,255)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            
+    game_window.fill(WHITE)
+    pygame.display.flip()
