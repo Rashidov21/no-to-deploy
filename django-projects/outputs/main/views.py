2a40fae392b217e12f722cf7bafa2117494187c9
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-01-20 19:53:56 +0500

    Update views.py

--- django-projects/outputs/main/views.py
+++ django-projects/outputs/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
+
+
 def register(request):
     if request.method == "POST":
         username = request.POST.get("username")
