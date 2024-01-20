58c44e8891dbab173d2c6a2e0e6aa86314d8cac1
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-01-20 19:54:24 +0500

    Update models.py

--- django-projects/outputs/main/models.py
+++ django-projects/outputs/main/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Limit(models.Model):
     limit = models.IntegerField(default=0)
     set_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.limit)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", null=True)
@@ -15,6 +17,7 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user.username)
 
+
 class Category(models.Model):
     title = models.CharField(max_length=100)
     emoji = models.CharField(max_length=100, blank=True)
@@ -22,6 +25,7 @@ class Category(models.Model):
 
     def __str__(self):
         return str(self.title)
+
 
 class Expense(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="expenses", null=True)
