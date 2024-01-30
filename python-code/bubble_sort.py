78b380e437e8f6d7a677a264c3babe17867bc080
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-01-30 11:07:33 +0500

    Update bubble_sort.py

--- python-code/bubble_sort.py
+++ python-code/bubble_sort.py
@@ -1,7 +1,10 @@
 arr = [5,7,4,3,8,2,9]
 print(arr)
-
-for i in range(len(arr)-1):
-    if arr[i] < arr[i+1]:
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+count = 0
+for iter in range(len(arr) - 1):
+    for i in range(len(arr)-1):
+        if arr[i] < arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            count += 1
 print(arr)
+print(count)
