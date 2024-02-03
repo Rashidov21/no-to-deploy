180097b6ebaf6b9fb55b073682c9e7f81501f0b8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-02-03 17:32:39 +0500

    Update urls.py

--- django-projects/devblog_backend/blog/urls.py
+++ django-projects/devblog_backend/blog/urls.py
@@ -6,3 +6,13 @@ app_name = 'blog'
 urlpatterns = [
     path("",views.post_list, name='post_list')
 ]
+
+
+arr = [1,2,3,4,5]
+
+for i in range(500):
+    arr.append(i)
+    
+l = list(range(500))
+
+l = list( range(2,500,2) )
