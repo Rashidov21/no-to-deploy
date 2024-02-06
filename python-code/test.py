97973f9aebe7ec977aa01986b838af2b133692f2
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-02-06 12:08:44 +0500

    Update test.py

--- python-code/test.py
+++ python-code/test.py
@@ -1,3 +1,15 @@
 # Pythonda map() funksiyasi 
 
+# map() - berilgan har bir ketma-ketlik elementlari 
+# uchun ma'lum bir funksiyani qo'llash 
+
+def square(num):
+    return int(num) ** 2
+
+arr = [1,2,3] # sonlarni darajalarini chiqaring
+
+print(map(square,arr))
+
+print(list(map(square,arr))) # [1, 4, 9]
+
 
