1dfdff86438f7b94a392f727e6bed2f236d50c0b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-02-06 11:07:32 +0500

    Update test.py

--- python-code/test.py
+++ python-code/test.py
@@ -1,12 +1,3 @@
-# Pythonda qatorlarni qirqish 
-text = "Assalomu alaykum !"
+# Pythonda map() funksiyasi 
 
-first_five_items = text[:5] # Assal
 
-last_five_items = text[-5:] # kum !
-
-range_n_items = text[2:8] # salomu
-
-print(first_five_items)
-print(last_five_items)
-print(range_n_items)
