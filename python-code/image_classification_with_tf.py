9bca3c53e111a90c32572f9997665fe8465391f7
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-02-19 17:03:34 +0500

    Create image_classification_with_tf.py

---
+++ python-code/image_classification_with_tf.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    layers.MaxPooling2D(2, 2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
