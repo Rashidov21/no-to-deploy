7970f52972b5d7f0704d1865540a978e4857ad69
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-02-22 18:19:48 +0500

    update

--- python-code/foto_downlaoder.py
+++ python-code/foto_downlaoder.py
@@ -3,10 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import wget
 
-url = "https://lifehacker.ru/nelepye-bannery-16-foto/" 
+url = "https://www.geeksandijan.uz/" 
 
-if not os.path.exists("images"):
-    os.makedirs("images")
+if not os.path.exists("geeks_images"):
+    os.makedirs("geeks_images")
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -21,7 +21,7 @@ for img in images:
 
     try:
         print(f"Yuklanmoqda: {img_url}")
-        wget.download(img_url, out="images/")
+        wget.download(img_url, out="geeks_images/")
         print("\nYuklab olindi!")
     except Exception as e:
         print(f"Xato: {e}")
