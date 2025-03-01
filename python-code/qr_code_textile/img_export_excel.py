8ef20f5052b643b37d77eec61f5140f723d6ecb9
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-03-01 11:49:17 +0500

    Update img_export_excel.py

--- python-code/qr_code_textile/img_export_excel.py
+++ python-code/qr_code_textile/img_export_excel.py
@@ -9,47 +9,31 @@ save_dir = "QR_codes"
 os.makedirs(save_dir, exist_ok=True)
 
 def is_qr_code(image):
-    # Преобразуем изображение в массив numpy
-    img_array = np.array(image.convert("L"))  # Переводим в градации серого
+    img_array = np.array(image.convert("L")) 
     detector = cv2.QRCodeDetector()
     data, _, _ = detector.detectAndDecode(img_array)
-    return bool(data)  # Если data не пустое — это QR-код
+    return bool(data)  # QR code out
 
 
 def extract_images_from_excel(excel_file_path, output_folder):
-    # Load the Excel workbook
     wb = load_workbook(excel_file_path, data_only=True)
     ws = wb.active
     if hasattr(ws, '_images') and ws._images:
         qr_count = 0
         for idx, image in enumerate(ws._images):
-            img_data = image._data()  # Получаем бинарные данные
-            img = Image.open(BytesIO(img_data))  # Открываем как изображение
+            img_data = image._data()  
+            img = Image.open(BytesIO(img_data))  
             if img.mode == "CMYK":
                 img = img.convert("RGB")
             if is_qr_code(img):
                 qr_count += 1
                 save_path = os.path.join(save_dir, f"qr_code_{qr_count}.png")  # Путь к файлу
-                img.save(save_path)  # Сохраняем в новую папку
+                img.save(save_path)  # Save image
                 print(f"Saved - {qr_count}")
     else:
         print("Изображения не найдены.")
 
-    # for sheet_name in workbook.sheetnames:
-    #     sheet = workbook[sheet_name]
-    #     print(sheet._drawing)
-    #     # Iterate over all drawings in the sheet
-    #     for drawing_id, drawing in enumerate(sheet._drawing, start=1):
-    #         if isinstance(drawing, openpyxl.drawing.image.Image):
-    #             # Extract the image data
-    #             image_data = drawing.image
-
-    #             # Save the image to the output folder
-    #             image_filename = f"{sheet_name}_image_{drawing_id}.png"
-    #             image_path = f"{output_folder}/{image_filename}"
-    #             image_data.save(image_path)
-
-    #             print(f"Image extracted: {image_path}")
+    wb.close()
 
 # Example usage
 excel_file_path = 'qr_code_textile/data.xlsx'
