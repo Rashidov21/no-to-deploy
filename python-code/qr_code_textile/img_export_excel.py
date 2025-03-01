b36def9fc170cc186eedc7af1cb2d9969f20effa
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-03-01 11:23:35 +0500

    Update img_export_excel.py

--- python-code/qr_code_textile/img_export_excel.py
+++ python-code/qr_code_textile/img_export_excel.py
@@ -1,6 +1,19 @@
+import os
+import cv2
+import numpy as np
 from openpyxl import load_workbook
 from PIL import Image
 from io import BytesIO
+
+save_dir = "QR_codes"
+os.makedirs(save_dir, exist_ok=True)
+
+def is_qr_code(image):
+    # Преобразуем изображение в массив numpy
+    img_array = np.array(image.convert("L"))  # Переводим в градации серого
+    detector = cv2.QRCodeDetector()
+    data, _, _ = detector.detectAndDecode(img_array)
+    return bool(data)  # Если data не пустое — это QR-код
 
 
 def extract_images_from_excel(excel_file_path, output_folder):
@@ -8,13 +21,17 @@ def extract_images_from_excel(excel_file_path, output_folder):
     wb = load_workbook(excel_file_path, data_only=True)
     ws = wb.active
     if hasattr(ws, '_images') and ws._images:
+        qr_count = 0
         for idx, image in enumerate(ws._images):
             img_data = image._data()  # Получаем бинарные данные
             img = Image.open(BytesIO(img_data))  # Открываем как изображение
             if img.mode == "CMYK":
                 img = img.convert("RGB")
-            img.save(f"extracted_image_{idx+1}.png")  # Сохраняем
-            print(f"Изображение {idx+1} сохранено.")
+            if is_qr_code(img):
+                qr_count += 1
+                save_path = os.path.join(save_dir, f"qr_code_{qr_count}.png")  # Путь к файлу
+                img.save(save_path)  # Сохраняем в новую папку
+                print(f"Saved - {qr_count}")
     else:
         print("Изображения не найдены.")
 
