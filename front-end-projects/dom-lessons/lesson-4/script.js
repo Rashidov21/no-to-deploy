bab8b23e8a56e4c0c16c702e83e5920371ad3588
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-05-01 10:27:56 +0500

    d

--- front-end-projects/dom-lessons/lesson-4/script.js
+++ front-end-projects/dom-lessons/lesson-4/script.js
@@ -1,14 +1,37 @@
 // Event - object userni harakatlarini kuzatish va unga mos javob berish obyekti 
 // click events 
 // onclick - chap tugma bosilganda 
-let counter = 0;
+// let counter = 0;
 // chap tugma 
-window.onclick = function(){
-    console.log(counter++)
-}
+// window.onclick = function(){
+//     console.log(counter++)
+// }
 // o'ng tugma 
-window.oncontextmenu = function(){
-    console.log(counter--)
-}
+// window.oncontextmenu = function(){
+//     console.log(counter--)
+// }
 // console.dir(window)
 // console.dir(document)
+// let box = document.querySelector(".box");
+// console.dir(box)
+// kursor ustiga kelganda
+// box.onmouseover = function(){
+//     box.style.width = "150px"
+// }
+// kursor ustidan ketganda
+// box.onmouseout = function(){
+//     box.style.width = "100px"
+// }
+// chap tugmani bosib turgan vaqtda 
+// box.onmousedown = function(){
+//     box.style.background = "khaki"
+// }
+// chap tugmani qoyib yuborgandan so'ng
+// box.onmouseup = function(){
+//     box.style.background = "blueviolet"
+// }
+// dragstart 
+// dragover
+// drop 
+
+const items = document.querySelectorAll(".item");
